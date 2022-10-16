4de2894376f696aff195f155abfc5f24be6ee4eb
KhangPhc <108499861+KhangPhc@users.noreply.github.com>
2022-10-16 10:45:18 +0700

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -10,8 +10,8 @@ local SettingsTable = SettingsLibrary.LoadSettings(FileName)
 local CrabHub = Material.Load({
 	Title = "CrabHub",
 	Style = 3,
-	SizeX = 5,
-	SizeY = 5,
+	SizeX = 500,
+	SizeY = 350,
 	Theme = "Light",
 })
 
